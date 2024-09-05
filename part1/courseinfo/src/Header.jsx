bfd036e891d62c3e3176007a1c034b5aa17c021b
Sonika Baniya <sonika.baniya.sb@gmail.com>
2024-09-05 18:29:07 +0200

    1.1

---
+++ part1/courseinfo/src/Header.jsx
@@ -0,0 +1,10 @@
+const Header = () => {
+    const course = 'Half Stack application development'
+    return (
+      <div>
+        <h1>{course}</h1>
+      </div>
+    )
+  }
+  
+  export default Header;
